f98dd00e229515915953f5271a72a70a7d2ad252
BenC <bcuttance1998@gmail.com>
2023-06-05 15:01:37 +0930

    added switchcase for some prompts. added function to add employess

--- Database/seeds.sql
+++ Database/seeds.sql
@@ -3,18 +3,21 @@ VALUES (1, "English"),
         (2, "Maths"),
         (3, "Science"),
         (4, "Media"),
-        (5, "Physical Ed");
+        (5, "Physical Ed")
+        (6, "Management");
 
 INSERT INTO role (id, title, salary, department_id)
 VALUES (101, "English Teacher", 50000, 1),
      (102, "Maths Teacher", 55000, 2), 
      (103, "Science Teacher", 60000, 3), 
      (104, "Media Teacher", 45000, 4),
-     (105, "P.E Teacher", 40000, 5);
+     (105, "P.E Teacher", 40000, 5),
+     (106, 'Principle', 99999999, 6);
 
 INSERT INTO employee (id, first_name, last_name, role_id, manager_id)
-VALUES (111, 'John', 'Doe', 101, 001),
-        (222, 'Walter', 'White', 102, 002), 
-        (333, 'Jon', 'Snow', 103, 003),
-        (444, 'Daryl', 'Dickson', 104, 004),
-        (555, 'Rick', 'Grimes', 105, 005);
+VALUES (111, 'John', 'Doe', 101, 106),
+        (222, 'Walter', 'White', 102, 106), 
+        (333, 'Jon', 'Snow', 103, 106),
+        (444, 'Daryl', 'Dickson', 104, 106),
+        (555, 'Rick', 'Grimes', 105, 106)
+        (666, 'Ben', 'Cuttance', 106, );
--- server.js
+++ server.js
@@ -1,5 +1,24 @@
 const inquirer = require('inquirer')
 const fs = require('fs')
+const mysql = require('mysql2');
+const express = require('express')
+
+const app = express();
+
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+// Connect to database
+
+const db = mysql.createConnection (
+    {
+        host: 'localhost',
+        user: 'root',
+        password: 'Cutty1998!',
+        database: 'database_db'
+    },
+    console.log('Connected to database')
+);
 
 // Prompts for quesions 
 
@@ -10,39 +29,84 @@ const questions = [
         choices: ['View All Employees', 'Add Employee', 'Update Employee Role', 'View All Roles', 'Add Role', 'View All Departments', 'Add Department', 'Exit'],
         name: 'opening'
     },
-    // {
-    //     type: 'list',
-    //     message: ,
-    //     choices: [],
-    //     name: ''
-    // }
 ]
+
+    const employeeAdd = [
+        {
+        type: 'input',
+        message: "What is the employee's first name?",
+        name: 'firstName'
+        },
+        {
+            type: 'input',
+            message: "What is the employee's last name?",
+            name: 'lastName'
+        },
+        {
+            type: 'list',
+            message: "What is the employee's role?",
+            choices: ['English Teacher', 'Maths Teacher', 'Science Teacher', 'Media Teacher', 'P.E Teacher', 'Principle'],
+            name: 'employeeRole'
+        }
+    ]
+function addEmployee () {
+    inquirer.prompt(employeeAdd).then(answers => {
+    db.query(`INSERT INTO employee (id, first_name, last_name, role_id, manager_id)
+     VALUES (999, '${answers.firstName}', '${answers.lastName}', 1, 106)`)
+        console.log(`${answers.firstName}, ${answers.lastName} added to list`)
+    }
+ )}
+
 
 
 const shutDown = () => {
     console.log('Shutting Down in...'),
-    setTimeout(() => {
-        console.log('3') }, 1000);
-    
-    setTimeout(() => {
-        console.log('2') }, 2000);
-       
-    setTimeout(() => {
-            console.log('1') }, 3000);
-           
-    setTimeout(() => {
-            console.log('Good Bye!') }, 4000);
+        setTimeout(() => {
+            console.log('3')
+        }, 1000);
 
-    }
+    setTimeout(() => {
+        console.log('2')
+    }, 2000);
 
+    setTimeout(() => {
+        console.log('1')
+    }, 3000);
 
+    setTimeout(() => {
+        console.log('Good Bye!')
+    }, 4000);
+
+}
 
 
 function ask() {
     inquirer.prompt(questions).then(answers => {
         switch (answers.opening) {
             case 'View All Employees': {
-                console.log('Employees Beep Boop'),
+              // NEED TO ADD DEPARTMENT AND SALARY TO QUERY
+                db.query('SELECT id AS ID, first_name As First_Name, last_name AS Last_Name FROM employee', function (err, results){
+                    console.log(results)
+                })
+                    ask(); break;
+            }
+            case 'View All Roles': {
+                // NEED TO ADD DEPARTMENT TO QUERY
+                db.query('SELECT id AS ID, title as Title, salary as Salary', function (err, results){
+                    console.log(results)
+                })
+                ask(); break; 
+            }
+            case 'Add Employee': {
+                // AUTO INCREMENT FEATURE?
+                addEmployee(); break;
+                
+            }
+            case 'View All Departments': {
+                
+                db.query('Select id as ID, name as Department FROM department', function (err, results){
+                    console.log(results)
+                });
                 ask(); break;
             }
             case 'Exit': {
@@ -50,9 +114,9 @@ function ask() {
                 return;
             }
             default: console.log('Default beep boop'),
-            ask(); break;
+                ask(); break;
         }
-        
+
     })
 }
 
